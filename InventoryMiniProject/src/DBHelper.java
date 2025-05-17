9c3c5ff182a91d9584f2f274191babf1d444b6ec
Riya-Singh-Hash <riyasingh979me@gmail.com>
2025-05-17 22:48:18 +0530

    Update DBHelper.java

--- InventoryMiniProject/src/DBHelper.java
+++ InventoryMiniProject/src/DBHelper.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DBHelper {
     private static final String URL = "jdbc:mysql://localhost:3306/InventoryDB";
     private static final String USER = "root"; // replace with your MySQL username
-    private static final String PASSWORD = "Topology@79"; // replace with your MySQL password
+    private static final String PASSWORD = "pswd"; // replace with your MySQL password
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
